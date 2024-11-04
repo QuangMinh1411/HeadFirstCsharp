341e0ab3ea8884f8a7c970e3001d676ea1ded8e2
quangminh <tomminhquan1411@gmail.com>
2024-11-04 17:08:26 +0700

    Variables,Loop,Methods

--- Chapter1/MyFirstConsoleApp/Program.cs
+++ Chapter1/MyFirstConsoleApp/Program.cs
@@ -1,2 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+OperatorExamples();
+
+void OperatorExamples()
+{
+    int width = 3;
+    width++;
+    int height = 2 + 4;
+    int area = width * height;
+    Console.WriteLine(area); 
+    while (area<50)
+    {
+        height++;
+        area = width * height;
+    }
+    do
+    {
+        width--;
+        area = width * height;
+    } while (area>25);
+    
+    
+    string result = "The area";
+    result = result + " is " + area;
+    Console.WriteLine(result);
+
+    bool truthValue = true;
+    Console.WriteLine(truthValue);
+}
+
+TryAnIf();
+TrySomeLoops();
+TryAnIfElse();
+
+static void TryAnIf()
+{
+    int someValue = 4;
+    string name = "Bobbo Jr.";
+    if ((someValue == 3) && (name == "Joe"))
+    {
+        Console.WriteLine("x is 3 and the name is Joe");
+    }
+    Console.WriteLine("this line runs no matter what");
+}
+
+static void TryAnIfElse()
+{
+    int x = 5;
+    if (x == 10)
+    {
+        Console.WriteLine("x must be 10");
+    }
+    else
+    {
+        Console.WriteLine("x isn’t 10");
+    }
+}
+
+static void TrySomeLoops()
+{
+    int count = 0;
+
+    while (count < 10)
+    {
+        count = count + 1;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        count = count - 1;
+    }
+
+    Console.WriteLine("The answer is " + count);
+}
