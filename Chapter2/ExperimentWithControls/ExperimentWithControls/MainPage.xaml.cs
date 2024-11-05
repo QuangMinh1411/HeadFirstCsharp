bec1b9c38041e32de6c8a86d5f501c45349dd78c
quangminh <tomminhquan1411@gmail.com>
2024-11-05 14:54:12 +0700

    Control sample

---
+++ Chapter2/ExperimentWithControls/ExperimentWithControls/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace ExperimentWithControls
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BirdPicker.ItemsSource = new string[] { "Duck", "Pigeon", "Penguin", "Ostrich", "Owl" };
+        }
+
+     
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnteredText.Text = e.NewTextValue;
+        }
+
+        private void Stepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            StepperValue.Text = e.NewValue.ToString();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            SliderValue.Text = e.NewValue.ToString();
+        }
+
+        private void AddBird_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(Birds.Text))
+            {
+                Birds.Text = Birds.Text + Environment.NewLine;
+            }
+            Birds.Text += BirdPicker.SelectedItem;
+        }
+    }
+
+}
